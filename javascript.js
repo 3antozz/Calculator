6f14b3e1e3061d294655408847b6b9cd03c49ce5
3antozz <106344848+3antozz@users.noreply.github.com>
2024-08-19 16:49:35 +0100

    add eventlistener to digit buttons to fill display

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+const buttonsContainer = document.querySelector(".buttons-container");
+const displayContainer = document.querySelector(".display");
+
+
+
+
+
+handleDisplayAndButtons();
+
 function add (a, b) {
     return a+b;
 }
@@ -17,6 +26,7 @@ function divide (a ,b) {
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue;
 
 function operate (firstNumber, operator, secondNumber) {
     switch (operator) {
@@ -32,4 +42,66 @@ function operate (firstNumber, operator, secondNumber) {
         case "/": 
             return divide(firstNumber, secondNumber);
     }
+}
+
+function handleDisplayAndButtons () {
+    displayContainer.textContent = 0;
+    buttonsContainer.addEventListener("click", (event) => {
+        if (displayContainer.textContent == 0 ) {
+            displayContainer.textContent = "";
+        }
+        switch (event.target.id) {
+            case "button-zero":
+                displayContainer.textContent += 0;
+                break;
+            
+            case "button-one":
+                displayContainer.textContent += 1;
+                break;
+
+            case "button-two":
+                displayContainer.textContent += 2;
+                break;
+
+            case "button-three":
+                displayContainer.textContent += 3;
+                break;
+
+            case "button-four":
+                displayContainer.textContent += 4;
+                break;
+
+            case "button-five":
+                displayContainer.textContent += 5;
+                break;
+
+            case "button-six":
+                displayContainer.textContent += 6;
+                break;  
+                
+            case "button-seven":
+                displayContainer.textContent += 7;
+                break;
+
+            case "button-eight":
+                displayContainer.textContent += 8;
+                break;
+
+            case "button-nine":
+                displayContainer.textContent += 9;
+                break;
+
+            case "button-clear":
+                displayContainer.textContent = 0;
+                break;
+
+        }
+        if (displayContainer.textContent.length > 16) {
+            displayContainer.textContent = displayContainer.textContent.substring(0, 16);
+        }
+
+        displayValue = +displayContainer.textContent;
+        
+    })
+
 }
