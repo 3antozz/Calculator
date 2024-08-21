4c68e205bcf8381756f2ac8a5fc016634b46df2d
3antozz <106344848+3antozz@users.noreply.github.com>
2024-08-21 16:41:14 +0100

    add keyboard compatiblity and replace duplicate code with functions

--- javascript.js
+++ javascript.js
@@ -1,9 +1,48 @@
 const buttonsContainer = document.querySelector(".buttons-container");
 const displayContainer = document.querySelector(".display");
 
+displayContainer.textContent = 0;
+let firstNumber = 0;
+let operator;
+let secondNumber = 0;
+let displayValue = 0;
+let resetDisplay = false;
+const keyMap = {
+    '0': 'button-zero',
+    '1': 'button-one',
+    '2': 'button-two',
+    '3': 'button-three',
+    '4': 'button-four',
+    '5': 'button-five',
+    '6': 'button-six',
+    '7': 'button-seven',
+    '8': 'button-eight',
+    '9': 'button-nine',
+    '+': 'button-add',
+    '-': 'button-substract',
+    '*': 'button-multiply',
+    '/': 'button-division',
+    'Enter': 'button-equal',
+    '=': 'button-equal',
+    ' ': 'button-equal',
+    'Backspace': 'button-backspace',
+    '.': 'button-decimal',
+    'Delete': 'button-clear'
+};
 
+buttonsContainer.addEventListener("click", (event) => {
+    const buttonID = event.target.id;
+    handleDisplayAndButtons(buttonID);
+})
+document.addEventListener("keydown", (event) => {
+    const buttonID = keyMap[event.key];
+    if (buttonID) {
+        handleDisplayAndButtons(buttonID);
+    }
+})
 
-handleDisplayAndButtons();
+// handleDisplayAndButtons();
+// handleKeyPresses();
 
 function add (a, b) {
     return (+a)+(+b);
@@ -25,12 +64,6 @@ function divide (a ,b) {
     return (+a)/(+b);
 }
 
-let firstNumber = 0;
-let operator;
-let secondNumber = 0;
-let displayValue = 0;
-let resetDisplay = false;
-
 function operate (firstNumber, operator, secondNumber) {
     switch (operator) {
         case "+":
@@ -46,183 +79,117 @@ function operate (firstNumber, operator, secondNumber) {
             return divide(firstNumber, secondNumber);
     }
 }
+function handleOperations(oper) {
+    if (operator) {
+        secondNumber = +displayValue;
+        firstNumber = displayContainer.textContent = operate(firstNumber, operator, secondNumber);
+    }
+    if (!operator && displayContainer.textContent == 0) {
+        displayContainer.textContent = "0";
+    }
+    firstNumber = +displayContainer.textContent;
+    displayValue = +displayContainer.textContent;
+    operator = oper;
+    secondNumber = +displayValue;
+    resetDisplay = true;
+}
 
-function handleDisplayAndButtons () {
+
+function handleDigits(digit) {
+    displayContainer.textContent += digit;
+    displayValue = +displayContainer.textContent;
+}
+
+function backspace () {
+    displayContainer.textContent = displayContainer.textContent.substring(0, displayContainer.textContent.length-1);
+    displayValue = +displayContainer.textContent;
+}
+
+
+function clear() {
     displayContainer.textContent = 0;
-    buttonsContainer.addEventListener("click", (event) => {
+    displayValue = +displayContainer.textContent;
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = "";
+}
+
+function decimal() {
+    if (displayContainer.textContent.length == 0) {
+        displayContainer.textContent += "0.";
+        displayValue = +displayContainer.textContent;
+    }
+
+    if (!displayContainer.textContent.includes(".")) {
+        displayContainer.textContent += ".";
+        displayValue = +displayContainer.textContent;
+    }
+}
+
+function equal () {
+    if (!operator && displayContainer.textContent == 0) {
+        return displayContainer.textContent = "0";
+    }
+    if (operator) {
+        secondNumber = +displayValue;
+        displayContainer.textContent = operate(firstNumber, operator, secondNumber);
+        firstNumber = +displayContainer.textContent;
+        secondNumber = +displayContainer.textContent;
+        displayValue = +displayContainer.textContent;
+        resetDisplay = true;
+        operator = "";
+    }
+} 
+
+function sign(){
+    displayContainer.textContent = +displayContainer.textContent * -1;
+    displayValue = +displayContainer.textContent;
+}
+
+
+function handleDisplayAndButtons (buttonID) {
+        let buttonElement = document.querySelector("#" + buttonID);
+
         if (displayContainer.textContent.includes("Rak kbir") || displayContainer.textContent == NaN || displayContainer.textContent === "ERROR" ){
             displayContainer.textContent = "0";
             displayValue = 0;
         }
-        if (displayContainer.textContent === "0" && event.target.id !== "button-decimal" ) {
+        if (displayContainer.textContent === "0" && buttonID !== "button-decimal" ) {
             displayContainer.textContent = "";
         }
-        if (resetDisplay && event.target.id !== "button-equal" && event.target.id !== "button-sign" && event.target.id !== "button-backspace" && operator) {
+        if (resetDisplay && buttonID !== "button-equal" && buttonID !== "button-sign" && buttonID !== "button-backspace" && operator) {
             displayContainer.textContent = "";
             resetDisplay = false;
         }
-        switch (event.target.id) {
-            case "button-zero":
-                displayContainer.textContent += 0;
-                displayValue = +displayContainer.textContent;
-                break;
-            
-            case "button-one":
-                displayContainer.textContent += 1;
-                displayValue = +displayContainer.textContent;
-                break;
 
-            case "button-two":
-                displayContainer.textContent += 2;
-                displayValue = +displayContainer.textContent;
-                break;
+        if (buttonElement.className === "digit"){ 
+            handleDigits(buttonElement.textContent);
+        }
 
-            case "button-three":
-                displayContainer.textContent += 3;
-                displayValue = +displayContainer.textContent;
-                break;
+        if (buttonElement.className === "operator"){ 
+            handleOperations(buttonElement.textContent);
+        }
 
-            case "button-four":
-                displayContainer.textContent += 4;
-                displayValue = +displayContainer.textContent;
-                break;
-
-            case "button-five":
-                displayContainer.textContent += 5;
-                displayValue = +displayContainer.textContent;
-                break;
-
-            case "button-six":
-                displayContainer.textContent += 6;
-                displayValue = +displayContainer.textContent;
-                break;  
-                
-            case "button-seven":
-                displayContainer.textContent += 7;
-                displayValue = +displayContainer.textContent;
-                break;
-
-            case "button-eight":
-                displayContainer.textContent += 8;
-                displayValue = +displayContainer.textContent;
-                break;
-
-            case "button-nine":
-                displayContainer.textContent += 9;
-                displayValue = +displayContainer.textContent;
-                break;
-
+        switch (buttonID) {
             case "button-clear":
-                displayContainer.textContent = 0;
-                displayValue = +displayContainer.textContent;
-                firstNumber = 0;
-                secondNumber = 0;
-                operator = "";
+                clear();
                 break;
 
             case "button-decimal":
-                if (displayContainer.textContent.length == 0) {
-                    displayContainer.textContent += "0.";
-                    displayValue = +displayContainer.textContent;
-                }
-
-                if (!displayContainer.textContent.includes(".")) {
-                    displayContainer.textContent += ".";
-                    displayValue = +displayContainer.textContent;
-                }
+                decimal();
                 break;
 
-
-                case "button-backspace":
-                    displayContainer.textContent = displayContainer.textContent.substring(0, displayContainer.textContent.length-1);
-                    displayValue = +displayContainer.textContent;
-                    break;
-
-                case "button-sign":
-                    displayContainer.textContent = +displayContainer.textContent * -1;
-                    displayValue = +displayContainer.textContent;
-                    break;
-
-            case "button-add":
-                if (operator) {
-                    secondNumber = +displayValue;
-                    firstNumber = displayContainer.textContent = operate(firstNumber, operator, secondNumber);
-                }
-                if (!operator && displayContainer.textContent == 0) {
-                    displayContainer.textContent = "0";
-                }
-                firstNumber = +displayContainer.textContent;
-                displayValue = +displayContainer.textContent;
-                operator = "+";
-                secondNumber = +displayValue;
-                resetDisplay = true;
+            case "button-backspace":
+                backspace();
                 break;
 
-            case "button-substract":
-                if (operator) {
-                    secondNumber = +displayValue;
-                    firstNumber = displayContainer.textContent = operate(firstNumber, operator, secondNumber);
-                }
-                if (!operator && displayContainer.textContent == 0) {
-                    displayContainer.textContent = "0";
-                }
-                firstNumber = +displayContainer.textContent;
-                displayValue = +displayContainer.textContent;
-                operator = "-";
-                secondNumber = +displayValue;
-                resetDisplay = true;
-                break;
-
-            case "button-multiply":
-                if (operator) {
-                    secondNumber = +displayValue;
-                    firstNumber = displayContainer.textContent = operate(firstNumber, operator, secondNumber);
-                }
-                if (!operator && displayContainer.textContent == 0) {
-                    displayContainer.textContent = "0";
-                }
-                firstNumber = +displayContainer.textContent;
-                displayValue = +displayContainer.textContent;
-                operator = "*";
-                secondNumber = +displayValue;
-                resetDisplay = true;
-                break; 
+            case "button-sign":
+                sign();
+                break;  
                 
-                
-
-            case "button-division":
-                if (operator) {
-                    secondNumber = +displayValue;
-                    firstNumber = displayContainer.textContent = operate(firstNumber, operator, secondNumber);
-                }
-                if (!operator && displayContainer.textContent == 0) {
-                    displayContainer.textContent = "0";
-                }
-                firstNumber = +displayContainer.textContent;
-                displayValue = +displayContainer.textContent;
-                operator = "/";
-                secondNumber = +displayValue;
-                resetDisplay = true;
-                break;    
-    
-            
-
             case "button-equal":
-                if (!operator && displayContainer.textContent == 0) {
-                    return displayContainer.textContent = "0";
-                }
-                if (operator) {
-                    secondNumber = +displayValue;
-                    displayContainer.textContent = operate(firstNumber, operator, secondNumber);
-                    firstNumber = +displayContainer.textContent;
-                    secondNumber = +displayContainer.textContent;
-                    displayValue = +displayContainer.textContent;
-                    resetDisplay = true;
-                    operator = "";
-                    break;
-                }
-
-
+                equal();
+                break;
         }
 
         if (displayContainer.textContent.includes("e")) {
@@ -244,6 +211,6 @@ function handleDisplayAndButtons () {
             displayContainer.textContent = "ERROR";
             displayValue = 0;
         }
-    })
+    
 
 }
